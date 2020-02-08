e8ae2dd5a0dbe0f57e2477091674e3e17ff17787
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2020-02-08 11:37:07 -0500

    refactor for configurestore

--- src/store.js
+++ src/store.js
@@ -1,13 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit'
 import reducer from './rootReducer'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, undefined, composeEnhancers(
-  applyMiddleware(
-    thunkMiddleware
-  )
-));
+const store = configureStore({
+  reducer
+})
 
 export default store;
