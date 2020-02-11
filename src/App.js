9ff059a83aa99753376b9aac5988d6ee539f74ed
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2020-02-10 23:11:22 -0500

    rename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
-import Login from './components/login/login.controller';
+import Login from './components/login/login.container';
 
 function App() {
   return (
