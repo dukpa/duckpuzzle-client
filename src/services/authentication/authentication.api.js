73a538da0728f30239fb631220e52a42bf7d9ee8
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2020-02-16 21:21:13 -0500

    added token handling

--- src/services/authentication/authentication.api.js
+++ src/services/authentication/authentication.api.js
@@ -2,6 +2,8 @@ import utils from '../../utils'
 
 export const BAD_USER_PASS = 'BAD_USER_PASS';
 
+const JWT_TOKEN_KEY = 'c9155b84-b3ba-4698-8d8c-b94524ae8a9f';
+
 export async function login(userName, password) {
   try {
     let resp = await utils.fetchJson('login', {
@@ -11,8 +13,21 @@ export async function login(userName, password) {
         password
       })
     });
+
+    if (resp.success) {
+      saveToken(resp.data.token);
+    }
+
     return resp;
   } catch(e) {
     console.error(e);
   }
+}
+
+function saveToken(token) {
+  localStorage.setItem(JWT_TOKEN_KEY, token);
+}
+
+export function getToken() {
+  return localStorage.getItem(JWT_TOKEN_KEY);
 }
