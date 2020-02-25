f91fcc43b175195a60137da9e4ea067f59d307eb
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2020-02-24 22:09:35 -0500

    fix lint

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {connect} from 'react-redux';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
 
